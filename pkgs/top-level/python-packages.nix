6bddc5a96f8b9bbeec5568c6f2f3747c42c57598
Robert Schütz <nix@dotlambda.de>
2021-12-31 10:48:48 +0000

    python3Packages.seaserv: use correct Python version
    (cherry picked from commit 23b5627b7a5f859ecadcab7087763cceb31694ef)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8525,7 +8525,9 @@ in {
 
   seahub = callPackage ../development/python-modules/seahub { };
 
-  seaserv = toPythonModule pkgs.seafile-server;
+  seaserv = toPythonModule (pkgs.seafile-server.override {
+    python3 = self.python;
+  });
 
   seccomp = callPackage ../development/python-modules/seccomp { };
 
