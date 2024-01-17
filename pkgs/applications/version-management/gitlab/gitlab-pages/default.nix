5f7a6402c9a5d3c2d30741cb7bc2e4157925d249
Yaya <github@uwu.is>
2024-01-17 14:31:00 +0100

    gitlab: 16.7.3 -> 16.8.0
    https://gitlab.com/gitlab-org/gitlab/-/blob/v16.8.0-ee/CHANGELOG.md

--- pkgs/applications/version-management/gitlab/gitlab-pages/default.nix
+++ pkgs/applications/version-management/gitlab/gitlab-pages/default.nix
@@ -2,17 +2,17 @@
 
 buildGoModule rec {
   pname = "gitlab-pages";
-  version = "16.7.3";
+  version = "16.8.0";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitlab-pages";
     rev = "v${version}";
-    hash = "sha256-KBG+pE0a12fGpzblS1sypjACxJNLsdmSGz6IociDKoA=";
+    hash = "sha256-p5avgQi0xnLRt3If7rnIWhq/TEwiZsyI5qLs5CVWots=";
   };
 
-  vendorHash = "sha256-NMky8v0YmN2pSeKJ7G0+DWAZvUx2JlwFbqPHvciYroM=";
+  vendorHash = "sha256-9yogTSdEij/YwWGm3gz7jMn2AZcI+o7aEnjBvJ8E3vA=";
   subPackages = [ "." ];
 
   meta = with lib; {
