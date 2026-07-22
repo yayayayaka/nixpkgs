34ba7b69365db1c436f8a1e0d85948a93853e92c
yaya <github@uwu.is>
2026-07-22 14:25:06 +0200

    gitlab-runner: 19.0.0 -> 19.2.0
    Diff: https://gitlab.com/gitlab-org/gitlab-runner/-/compare/v19.0.0...v19.2.0
    
    Changelog: https://gitlab.com/gitlab-org/gitlab-runner/blob/v19.2.0/CHANGELOG.md

--- pkgs/by-name/gi/gitlab-runner/package.nix
+++ pkgs/by-name/gi/gitlab-runner/package.nix
@@ -12,16 +12,16 @@
 
 buildGoModule (finalAttrs: {
   pname = "gitlab-runner";
-  version = "19.0.0";
+  version = "19.2.0";
 
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "gitlab-runner";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-uScTzj4pSRtSFCMxoOR5KqipCbPknwnydPYG6xU5dOo=";
+    hash = "sha256-aWX506nR7yPHWIXBQq/Mwu+EFoa4DYKCjKNT74A85xs=";
   };
 
-  vendorHash = "sha256-QqqTkIgR9ca1dYQ32SG7C+SpEIA07Hlf8x3lVhZ5vRQ=";
+  vendorHash = "sha256-kznAIKiJUZUkoNQSsnl6pj4SZspErccc6rmGwuc7iKo=";
 
   # For patchShebangs
   buildInputs = [ bash ];
